c76324b3602068afbf0b195b91a50ec462b07802
dhanrajsingh03 <dhanrajku7856@gmail.com>
2025-09-26 01:20:06 +0530

    final commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Bachelor Degree in Electronics Engineering",
+    duration: "2025 - Present",
+    institution: "Government Engineering College, Buxar",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Secondary School Education",
+    duration: "2020 - 2022",
+    institution: "M.D. High School, Singheshwar",
+  },
+  {
+    id: 2,
+    title: "3 Years Diploma in Engineering (Electronics & Communication)",
+    duration: "2022-2025",
+    institution: "New Government Polytechnic Patna 13",
   }
 ]
